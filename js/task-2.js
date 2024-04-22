18a190a488dfbc91bc2f7672b0c1d5ee84d00f60
Zaverzaiev Anton <crossya94@gmail.com>
2024-04-22 21:39:43 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+function formatMessage(message, maxLength) {
+  const x =
+    message.length <= maxLength
+      ? `${message}`
+      : `${message.slice(0, maxLength)}...`;
+  return x;
+}
+
+formatMessage('Curabitur ligula sapien', 16); // "Curabitur ligula..."
+formatMessage('Curabitur ligula sapien', 23); // "Curabitur ligula sapien"
+formatMessage('Vestibulum facilisis purus nec', 20); // "Vestibulum facilisis..."
+formatMessage('Vestibulum facilisis purus nec', 30); // "Vestibulum facilisis purus nec"
+formatMessage('Nunc sed turpis a felis in nunc fringilla', 15); // "Nunc sed turpis..."
+formatMessage('Nunc sed turpis a felis in nunc fringilla', 41); // "Nunc sed turpis a felis in nunc fringilla"
+
+console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
+console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
+console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
+console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
