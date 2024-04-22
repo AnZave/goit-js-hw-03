65adeef5506a26c02f180937c0d6c7f9ee2e6731
AnZave <crossya94@gmail.com>
2024-04-22 22:16:59 +0300

    hw3

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,19 @@
-function formatMessage(message, maxLength) {
-  const x =
-    message.length <= maxLength
-      ? `${message}`
-      : `${message.slice(0, maxLength)}...`;
-  return x;
+function makeArray(firstArray, secondArray, maxLength) {
+  // Спочатку створюємо новий масив, який є об'єднанням firstArray і secondArray
+  const copyArray = firstArray.concat(secondArray);
+  // Якщо кількість елементів перевищує maxLength, повертаємо обмежений масив
+  if (copyArray.length >= maxLength) {
+    return copyArray.slice(0, maxLength);
+  }
+  // Інакше повертаємо весь масив
+  else {
+    return copyArray;
+  }
 }
 
-formatMessage('Curabitur ligula sapien', 16); // "Curabitur ligula..."
-formatMessage('Curabitur ligula sapien', 23); // "Curabitur ligula sapien"
-formatMessage('Vestibulum facilisis purus nec', 20); // "Vestibulum facilisis..."
-formatMessage('Vestibulum facilisis purus nec', 30); // "Vestibulum facilisis purus nec"
-formatMessage('Nunc sed turpis a felis in nunc fringilla', 15); // "Nunc sed turpis..."
-formatMessage('Nunc sed turpis a felis in nunc fringilla', 41); // "Nunc sed turpis a felis in nunc fringilla"
-
-console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); //
